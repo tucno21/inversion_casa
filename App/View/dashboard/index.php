09003905c31e5fe054b15524573f9c5cb0aa4a41
tucno21 <carlitostucno@gmail.com>
2021-11-02 11:21:56 -0500

    cambio de tabla

--- App/View/dashboard/index.php
+++ App/View/dashboard/index.php
@@ -8,39 +8,40 @@
         <a class="btn border border-dark rounded" href="#">Total: S/<?= isset($suma) ? number_format($suma, 2., '.', ',') : '0' ?></a>
     </div>
     <div class="card-body">
-
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Registrado:</th>
-                    <th>Descripción</th>
-                    <th>Cantidad</th>
-                    <th>Precio</th>
-                    <th>Total</th>
-                    <th>F-registro</th>
-                    <th>Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($inversiones as $inv) : ?>
+        <div class="table-responsive-sm">
+            <table class="table table-sm table-bordered table-striped">
+                <thead>
                     <tr>
-                        <th scope="row"><?= $inv->id ?></th>
-                        <td><?= $inv->id_user ?></td>
-                        <td><?= $inv->features ?></td>
-                        <td><?= $inv->cant ?></td>
-                        <td><?= $inv->price ?></td>
-                        <td>S/<?= number_format(($inv->price * $inv->cant), 2., '.', ',') ?></td>
-                        <td><?= $inv->created_at ?></td>
-                        <td>
-                            <button url="<?= base_url('/dashboard/ver?id=' . $inv->id) ?>" class="btn btn-info verArchivo"><i class="fas fa-eye"></i></button>
-                            <button url="<?= base_url('/dashboard/edit?id=' . $inv->id) ?>" class="btn btn-warning alertEdit"><i class="fas fa-edit"></i></button>
-                            <button url="<?= base_url('/dashboard/delete?id=' . $inv->id) ?>" class="btn btn-danger alertDelete"><i class="fas fa-trash"></i></button>
-                        </td>
+                        <th>#</th>
+                        <th>Registrado:</th>
+                        <th>Descripción</th>
+                        <th>Cantidad</th>
+                        <th>Precio</th>
+                        <th>Total</th>
+                        <th>F-registro</th>
+                        <th>Acciones</th>
                     </tr>
-                <?php endforeach ?>
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    <?php foreach ($inversiones as $inv) : ?>
+                        <tr>
+                            <th scope="row"><?= $inv->id ?></th>
+                            <td><?= $inv->id_user ?></td>
+                            <td><?= $inv->features ?></td>
+                            <td><?= $inv->cant ?></td>
+                            <td><?= $inv->price ?></td>
+                            <td>S/<?= number_format(($inv->price * $inv->cant), 2., '.', ',') ?></td>
+                            <td><?= $inv->created_at ?></td>
+                            <td>
+                                <button url="<?= base_url('/dashboard/ver?id=' . $inv->id) ?>" class="btn btn-info verArchivo"><i class="fas fa-eye"></i></button>
+                                <button url="<?= base_url('/dashboard/edit?id=' . $inv->id) ?>" class="btn btn-warning alertEdit"><i class="fas fa-edit"></i></button>
+                                <button url="<?= base_url('/dashboard/delete?id=' . $inv->id) ?>" class="btn btn-danger alertDelete"><i class="fas fa-trash"></i></button>
+                            </td>
+                        </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 
